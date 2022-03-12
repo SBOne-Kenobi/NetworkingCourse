7249eccde83d4b52791e4cdf56dea51ace7a5ff1
Rustam Sadykov <sbonekenobi@gmail.com>
2022-03-12 18:36:29 +0300

    hw3 - A

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
     application
 }
 
@@ -17,10 +16,6 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test-junit"))
-    implementation("io.ktor:ktor-server-netty:1.5.2")
-    implementation("io.ktor:ktor-html-builder:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
-    implementation("io.ktor:ktor-serialization:1.5.2")
 }
 
 tasks.test {
--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,28 +1,77 @@
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.routing.*
-import io.ktor.serialization.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import kotlinx.serialization.json.Json
-import routes.productRoutes
+import java.io.*
+import java.net.ServerSocket
 
-fun Application.module() {
-    install(ContentNegotiation) {
-        json(Json {
-            prettyPrint = true
-            isLenient = true
-        })
+data class HttpRequest(val method: String, val headers: Map<String, String>, val body: String)
+
+fun buildResponse(code: Int, message: String, body: String? = null) =
+    if (body == null)
+        """HTTP/1.1 $code $message
+
+        """.trimIndent()
+    else
+        """HTTP/1.1 $code $message
+Content-Type: text/html
+Content-Length: ${body.length}
+
+$body""".trimIndent()
+
+fun parseRequest(input: BufferedReader): HttpRequest? {
+    val startLine = input.readLine()
+    val method = startLine.split(" ").getOrNull(0)
+        ?: return null
+    val headers = hashMapOf<String, String>()
+    var length = 0
+    while (true) {
+        val line = input.readLine()
+        if (line.isBlank())
+            break
+        val idx = line.indexOfFirst { it == ':' }
+        if (idx == -1)
+            return null
+        val name = line.take(idx)
+        val value = line.drop(idx + 2)
+        headers[name] = value
+        if (name == "Content-Length") {
+            length = value.toIntOrNull()
+                ?: return null
+        }
     }
-    install(Routing)
-    productRoutes()
+    val body = CharArray(length)
+    input.read(body)
+    return HttpRequest(method, headers, String(body))
+}
+
+fun formResponseBody(request: HttpRequest): String? {
+    val file = File(request.body)
+    return if (file.exists())
+        file.readText()
+    else
+        null
+}
+
+fun server() {
+    val serverPort = 8080
+    val serverSocket = ServerSocket(serverPort)
+    while (true) {
+        val clientSocket = serverSocket.accept()
+        val output = PrintWriter(clientSocket.getOutputStream(), true)
+        val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+
+        val request = parseRequest(input)
+
+        val response = if (request == null) {
+            buildResponse(400, "Bad request", "")
+        } else {
+            formResponseBody(request)?.let {
+                buildResponse(200, "OK", it)
+            } ?: buildResponse(404, "Not found")
+        }
+        output.println(response)
+
+        clientSocket.close()
+    }
 }
 
 fun main() {
-    embeddedServer(
-        Netty, port = 8080,
-        host = "127.0.0.1"
-    ) {
-        module()
-    }.start(wait = true)
+    server()
 }
