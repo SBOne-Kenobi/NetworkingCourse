3dc14d016ef3e85f2bf4f00babdde695f67e1621
Rustam Sadykov <sbonekenobi@gmail.com>
2022-04-21 16:29:23 +0300

    make 9.1, 9.2

---
+++ src/main/kotlin/ipUtil.kt
@@ -0,0 +1,36 @@
+import java.net.InterfaceAddress
+import java.net.NetworkInterface
+import kotlin.streams.toList
+
+fun getInterfaceAddress(): InterfaceAddress {
+    return NetworkInterface
+        .networkInterfaces()
+        .toList()
+        .filter {
+            it.isUp && !it.isLoopback
+        }
+        .flatMap {
+            it.interfaceAddresses
+        }
+        .first {
+            it.address.hostAddress.startsWith("192.168")
+        }
+}
+
+val InterfaceAddress.ip: String
+    get() = address.hostAddress
+
+val InterfaceAddress.subnetwork: String
+    get() = (0..3).map { i ->
+        (8 - (networkPrefixLength - i * 8).coerceIn(0, 8)).let {
+            UByte.MAX_VALUE.toInt().shr(it).shl(it)
+        }
+    }.joinToString(".")
+
+fun main() {
+    val interfaceAddress = getInterfaceAddress()
+    val ipAddress = interfaceAddress.ip
+    val subnetwork = interfaceAddress.subnetwork
+    println("Ip address: $ipAddress")
+    println("Subnetwork mask: $subnetwork")
+}
