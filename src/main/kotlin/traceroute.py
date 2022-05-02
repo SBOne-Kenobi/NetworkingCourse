5dfb0478aff321e0d451f47efefb7dc3f91e6bbb
Rustam Sadykov <sbonekenobi@gmail.com>
2022-05-02 13:09:53 +0300

    add traceroute

--- src/main/kotlin/traceroute.py
+++ src/main/kotlin/traceroute.py
@@ -2,9 +2,9 @@ import argparse
 import os
 import socket
 import struct
-from enum import Enum
-from socket import socket, IPPROTO_ICMP, gethostbyname, AF_INET, SOCK_RAW, gaierror, IPPROTO_IP, IP_TTL
-from time import time, sleep
+import sys
+from socket import socket, IPPROTO_ICMP, gethostbyname, AF_INET, SOCK_RAW, gaierror, IPPROTO_IP, IP_TTL, getfqdn
+from time import time
 from typing import Optional
 
 from select import select
@@ -14,8 +14,6 @@ ICMP_ECHO_FORMAT = 'bbHHh'
 ICMP_ECHO_HEADER_SIZE = 8
 
 IP_HEADER_SIZE = 20
-
-current_seq = 1
 
 
 def calculate_checksum(data: bytes, reverse: bool = True) -> int:
@@ -49,25 +47,20 @@ def build_echo_packet(packet_id: int, seq: int) -> bytes:
 def parse_icmp(raw_data: bytes) -> tuple[int, int, any]:
     icmp_part = raw_data[IP_HEADER_SIZE:]
     icmp_type, icmp_code = struct.unpack('bb', icmp_part[:2])
-    if icmp_type == 0 and icmp_code == 0:
+    if (icmp_type == 0 or icmp_type == 8) and icmp_code == 0:
         icmp_header = icmp_part[:ICMP_ECHO_HEADER_SIZE]
         data = icmp_part[ICMP_ECHO_HEADER_SIZE:]
         icmp_type, icmp_code, checksum, p_id, s = struct.unpack(ICMP_ECHO_FORMAT, icmp_header)
         return icmp_type, icmp_code, (checksum, p_id, s, data)
-    elif icmp_type == 3:
-        return icmp_type, icmp_code, icmp_part[34:34 + 64]
+    elif icmp_type == 11:
+        checksum = struct.unpack('H', icmp_part[2:4])[0]
+        return icmp_type, icmp_code, (checksum, icmp_part[8:])
     else:
         return icmp_type, icmp_code, icmp_part[2:]
 
 
-class Error(Enum):
-    TIMEOUT = -1
-    NETWORK_UNREACHABLE = 0
-    HOST_UNREACHABLE = 1
-
-
 def receive_ping(sock: socket, packet_id: int, seq: int, time_sent: float, timeout: float) \
-        -> tuple[float, Optional[Error]]:
+        -> tuple[Optional[float], Optional[str], bool]:
     time_left = timeout
     while time_left > 0:
         select_time = time()
@@ -75,7 +68,7 @@ def receive_ping(sock: socket, packet_id: int, seq: int, time_sent: float, timeo
         select_time = time() - select_time
         if not ready[0]:
             break
-        rec_packet, _ = sock.recvfrom(1024)
+        rec_packet, (addr, _) = sock.recvfrom(1024)
         time_received = time()
         icmp_type, icmp_code, other = parse_icmp(rec_packet)
         if icmp_type == 0 and icmp_code == 0:
@@ -84,83 +77,69 @@ def receive_ping(sock: socket, packet_id: int, seq: int, time_sent: float, timeo
             if p_id == packet_id and \
                     s == seq and \
                     validate_checksum(data_for_checksum, checksum):
-                return time_received - time_sent, None
-        elif icmp_type == 3:
+                return time_received - time_sent, addr, True
+        elif icmp_type == 11:
+            checksum, other = other
             _, _, (_, p_id, s, _) = parse_icmp(other)
-            if p_id == packet_id and s == seq:
-                return time_received - time_sent, Error(icmp_code)
+            data_for_checksum = struct.pack('bbHxxxx', icmp_type, icmp_code, 0) + other
+            if p_id == packet_id and \
+                    s == seq and \
+                    validate_checksum(data_for_checksum, checksum):
+                return time_received - time_sent, addr, False
         time_left -= select_time
-    return time() - time_sent, Error.TIMEOUT
+    return None, None, False
 
 
-def ping(sock: socket, host: str, timeout: float = 1) -> tuple[float, Optional[Error], int]:
-    global current_seq
+def trace(sock: socket, host: str, timeout: float, n: int):
     packet_id = os.getpid() & 0xffff
-    seq = current_seq
-    current_seq += 1
-    packet = build_echo_packet(packet_id, seq)
-    sent = len(packet)
-    sock.sendto(packet, (host, 1))
-    rtt, err = receive_ping(sock, packet_id, seq, time(), timeout)
-    return rtt, err, sent
-
-
-class Statistics:
-
-    def __init__(self):
-        self.min_rtt = None
-        self.max_rtt = None
-        self.avg_rtt = None
-        self.cnt = 0
-        self.success_cnt = 0
-
-    def update(self, rtt: Optional[int]):
-        self.cnt += 1
-        if rtt is not None:
-            self.success_cnt += 1
-            if self.min_rtt is None:
-                self.min_rtt = self.max_rtt = self.avg_rtt = rtt
+    ttl = 0
+    seq = 0
+    while True:
+        ttl += 1
+        print(ttl, end='\t')
+        sock.setsockopt(IPPROTO_IP, IP_TTL, ttl)
+        host_reached = False
+        addr = None
+        for _ in range(n):
+            seq += 1
+            packet = build_echo_packet(packet_id, seq)
+            sock.sendto(packet, (host, 1))
+            rtt, r_addr, is_host = receive_ping(sock, packet_id, seq, time(), timeout)
+            if rtt is None:
+                print('*', end='\t')
             else:
-                self.min_rtt = min(self.min_rtt, rtt)
-                self.max_rtt = max(self.max_rtt, rtt)
-                self.avg_rtt = self.avg_rtt + (rtt - self.avg_rtt) / self.success_cnt
-
-    def __str__(self):
-        return f'Packages sent: {self.cnt}, Packages received: {self.success_cnt} ' \
-               f'({100 * (self.cnt - self.success_cnt) / self.cnt}% loss)' \
-               f'\n' \
-               f'Min rtt: {self.min_rtt}, Max rtt: {self.max_rtt}, Avg rtt: {self.avg_rtt}'
+                print(round(rtt * 1000), 'ms', end='\t')
+            sys.stdout.flush()
+            addr = r_addr or addr
+            if is_host:
+                host_reached = True
+        if addr is None:
+            print('*')
+        else:
+            name = getfqdn(addr)
+            if name == addr:
+                print(addr)
+            else:
+                print(f'{name} [{addr}]')
+        sys.stdout.flush()
+        if host_reached:
+            break
 
 
 def main(args):
     sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
     timeout = args.w / 1000
-    stat = Statistics()
-    if args.i is not None:
-        sock.setsockopt(IPPROTO_IP, IP_TTL, args.i)
     try:
         host = gethostbyname(args.host)
-        for _ in range(args.n):
-            rtt_sec, err, sent = ping(sock, host, timeout)
-            if err is None:
-                rtt = round(rtt_sec * 1000)
-                print(f'rtt: {rtt}ms, sent: {sent}b')
-                stat.update(rtt)
-            else:
-                print('Error:', err.name)
-                stat.update(None)
-            if rtt_sec is not None and rtt_sec < 1:
-                sleep(1 - rtt_sec)
-        print(stat)
+        trace(sock, host, timeout, args.n)
     except gaierror:
         print('Unknown host')
     sock.close()
 
 
-parser = argparse.ArgumentParser(description='Ping')
-parser.add_argument('-n', default=10, help='number of requests', type=int)
+parser = argparse.ArgumentParser(description='Trace route')
 parser.add_argument('-w', default=1000, help='timeout, ms', type=int)
-parser.add_argument('-i', default=None, help='TTL', type=int)
+parser.add_argument('-n', default=3, help='number of requests', type=int)
 parser.add_argument('host', help='host address', type=str)
 parsed_args = parser.parse_args()
 main(parsed_args)
